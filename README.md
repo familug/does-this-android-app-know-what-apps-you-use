5da6adc45710ccad13ba6c015ef5761f22121564
Viet Hung Nguyen <hvn@familug.org>
2025-05-10 22:59:03 +0900

    Add first version

--- README.md
+++ README.md
@@ -1,1 +1,59 @@
 # does-this-android-app-know-what-apps-you-use
+
+Blog <https://peabee.substack.com/p/everyone-knows-what-apps-you-use> phát hiện ra nhiều Android app đã lách cơ chế của Play Store để có thể xem các app nào cài trên máy. Gồm 2 cơ chế trong file AndroidManifest.xml:
+
+- liệt kê cụ thể từng app ra qua package id
+- khai báo `queries android.intent.action.MAIN` để có thể list tất cả app đã cài
+
+```
+<queries>
+  [...]
+  <intent>
+    <action android:name="android.intent.action.MAIN" />
+  </intent>
+  [...]
+</queries>
+```
+
+Repo này cung cấp công cụ để tìm hiểu các app nào có các quyền nói trên.
+
+## Mục tiêu
+Liệt kê top 100 app phổ biến nhất ở Google Play store Việt Nam.
+
+Xem kết quả tại [apps.csv](./apps.csv)
+
+## Cài đặt
+Trên Ubuntu, tải repo về, chạy cài đặt
+
+Tested on Ubuntu 22.04
+Requirements:
+
+```
+sudo apt install -y appt
+pip install --upgrade pip; pip install playwright ; playwright install
+```
+
+## Đóng góp
+
+Fork repo này.
+
+### Chạy
+Vào https://play.google.com tìm app cần kiểm tra, ví dụ facebook
+
+```
+ python main.py  --url 'https://play.google.com/store/apps/details?id=com.facebook.katana'
+
+ git checkout -b facebook
+ git add manifests apps.csv
+ git commit -m 'Add facebook'
+ git push origin facebook
+
+
+```
+Tạo PR
+
+## TODO
+- thêm cột các package cụ thể "được" theo dõi
+- sửa code để chạy được với headless browser, rồi tạo dockerfile để chạy trên docker/github action
+- tải app trực tiếp từ Google Play Store
+- tạo website check trực tiếp theo yêu cầu
---
+++ main.py
@@ -0,0 +1,251 @@
+"""
+Detects if given android app knows what apps you use.
+
+https://peabee.substack.com/p/everyone-knows-what-apps-you-use
+
+
+Tested on Ubuntu 22.04
+Requirements:
+
+sudo apt install -y appt
+pip install --upgrade pip; pip install playwright ; playwright install
+
+Visit https://apkpure.com/ find app click download, copy the link
+
+usage:
+
+python3 main.py -u 'https://d.apkpure.com/b/XAPK/com.shopee.vn?version=latest'
+
+TODO, PROMPT:
+
+- Get list of top app on Google Play Store , e.g in Vietnam
+- Run this check for each of them, could implement as a test return true/false
+- Create a table show the result
+- Save the test result as history
+- Try apttool to extrace the text XML file, it failed with vn shopeee app.
+"""
+
+import csv
+import datetime
+import subprocess
+import shutil
+import tempfile
+import os
+from typing import Any
+from playwright.sync_api import sync_playwright
+
+import argparse
+import urllib.parse as up
+
+
+def parse_id_from_play_url(url: str) -> str:
+    q = up.urlparse(url).query
+
+    return up.parse_qs(q)["id"][0]
+
+
+KISS_GPLAY_URL = "https://play.google.com/store/apps/details?id=fr.neamar.kiss"
+assert parse_id_from_play_url(KISS_GPLAY_URL) == "fr.neamar.kiss"
+# one of the smallest useful package ~1MB, use for quick test
+KISS_APK = "https://d.apkpure.com/b/XAPK/fr.neamar.kiss?version=latest"
+argp = argparse.ArgumentParser()
+argp.add_argument("--url", "-u", required=False, default=KISS_GPLAY_URL)
+argp.add_argument("--cleanup", "-c", required=False, action="store_true")
+argp.add_argument("--check", help="Check if leak base on given manifest dump")
+args = argp.parse_args()
+
+playwright = sync_playwright().start()
+# Use playwright.chromium, playwright.firefox or playwright.webkit
+# Pass headless=False to launch() to see the browser UI
+# browser = playwright.chromium.launch(headless=False, slow_mo=50)
+
+
+def gplay_to_download_url(gplay_url: str) -> str:
+    package_id = parse_id_from_play_url(gplay_url)
+
+    return f"https://d.apkpure.com/b/XAPK/{package_id}?version=latest"
+
+
+assert (
+    gplay_to_download_url(
+        "https://play.google.com/store/apps/details?id=fr.neamar.kiss"
+    )
+    == KISS_APK
+)
+
+url = gplay_to_download_url(args.url)
+
+
+# TODO: make it work with headless=True to able run on docker/GHA
+# TODO:  Try to download from other sources/directly from GPlay
+def download(url: str):
+    # https://github.com/microsoft/playwright-python/issues/1557
+    browser = playwright.chromium.launch(headless=False)
+    page = browser.new_page()
+    with page.expect_download() as download_info:
+        try:
+            page.goto(url)
+            print(page)
+        except Exception:
+            pass
+
+    download = download_info.value
+    return download
+
+
+manifest_dir = os.path.join(os.path.abspath(os.path.dirname(".")), "manifests")
+os.makedirs(manifest_dir, exist_ok=True)
+cur_dir = os.path.abspath(os.path.dirname("."))
+
+d = tempfile.mkdtemp()
+
+
+def extract_appname(suggested_filename: str) -> str:
+    return suggested_filename.removesuffix(".apk").split("_")[0]
+
+
+assert extract_appname("KISS Launcher_3.21.3_APKPure.apk") == "KISS Launcher"
+
+
+def extract(download: Any) -> dict[str, str]:
+    download_filepath = download.path()
+    print(download_filepath)
+    os.chdir(d)
+    os.system(f"cp {download_filepath} file.zip")
+    result: dict[str, str] = {}
+    if download.suggested_filename.endswith(".xapk"):
+        os.system("unzip " + str(download_filepath))
+        for fn in os.listdir():
+            if fn.endswith(".apk") and not fn.startswith("config."):
+                output = subprocess.run(
+                    [
+                        "aapt",
+                        "dump",
+                        "xmltree",
+                        fn,
+                        "AndroidManifest.xml",
+                    ],
+                    capture_output=True,
+                ).stdout.decode("utf-8")
+                package_id = parse_package_name(output)
+                if package_id in fn:
+                    result[extract_appname(download.suggested_filename)] = output
+
+    else:
+        output = subprocess.run(
+            ["aapt", "dump", "xmltree", download_filepath, "AndroidManifest.xml"],
+            capture_output=True,
+        ).stdout.decode("utf-8")
+        result[extract_appname(download.suggested_filename)] = output
+
+    return result
+
+
+def parse_version(manifest_dump: str) -> str:
+    """
+    A: android:versionName(0x0101021c)="3.49.41" (Raw: "3.49.41")
+    """
+    for line in manifest_dump.splitlines():
+        if "android:versionName" in line:
+            return line.split('"')[1]
+    return "unknown"
+
+
+assert (
+    parse_version("""A: android:versionName(0x0101021c)="3.49.41" (Raw: "3.49.41")""")
+    == "3.49.41"
+)
+
+
+def play_store_url(package_id: str) -> str:
+    return f"https://play.google.com/store/apps/details?id={package_id}"
+
+
+def parse_package_name(manifest_dump: str) -> str:
+    for line in manifest_dump.splitlines():
+        if "package=" in line:
+            return line.split('"')[1]
+    return "unknown"
+
+
+assert (
+    parse_package_name(""" A: package="com.shopee.vn" (Raw: "com.shopee.vn") """)
+    == "com.shopee.vn"
+)
+
+
+def get_file_last_modified(fn: str) -> str:
+    return datetime.datetime.fromtimestamp(os.stat(fn).st_mtime).strftime("%Y%m%d")
+
+
+def check_leak_query_packages(manifest: str) -> bool:
+    lines = manifest.splitlines()
+    start_idx = None
+    end_idx = None
+    for idx, line in enumerate(lines):
+        if ("    E: queries") in line:
+            start_idx = idx
+            indents = line.count(" ", 0, line.index("E:"))
+        elif start_idx and line.startswith(indents * " " + "E:"):
+            end_idx = idx
+            break
+    if end_idx is None:
+        end_idx = idx
+
+    if start_idx is not None and end_idx is not None:
+        is_leak = "android.intent.action.MAIN" in "".join(lines[start_idx:end_idx])
+        return is_leak
+
+    return False
+
+
+def summary() -> None:
+    headers = ["checked_at", "app_name", "app_id", "version", "knows_all", "store URL"]
+    with open(os.path.join(cur_dir, "apps.csv"), "wt") as csvf:
+        writer = csv.writer(csvf)
+        writer.writerow(headers)
+
+        for fn in sorted(os.listdir(manifest_dir)):
+            file_path = os.path.join(manifest_dir, fn)
+            with open(file_path) as f:
+                content = f.read()
+            leak = check_leak_query_packages(content)
+            # app_id = "_".join(fn.split("_")[:-1])
+            app_id = parse_package_name(content)
+            app_name = fn.split("_")[0]
+            version = parse_version(content)
+            store_url = play_store_url(app_id)
+            modified_time = get_file_last_modified(file_path)
+
+            writer.writerow([modified_time, app_name, app_id, version, leak, store_url])
+
+
+def run() -> None:
+    if args.check:
+        with open(args.check) as f:
+            print(check_leak_query_packages(f.read()))
+        exit(0)
+
+    download_res = download(url)
+    res = extract(download_res)
+    for fn, value in res.items():
+        output_fn = fn.removesuffix(".apk") + "_AndroidManifest.xmldump"
+
+        output_path = os.path.join(manifest_dir, output_fn)
+        with open(output_path, "wt") as f:
+            f.write(value)
+
+    summary()
+
+    if args.cleanup:
+        shutil.rmtree(d)
+
+
+def main() -> None:
+    run()
+
+
+main()
+# os.system("unzip " + str(download_filepath))
+# browser.close()
+# playwright.stop()
